af2aaa497c5874d7fdbca307ad65790f8b5c2e6c
Tom <tyohin2016@163.com>
2025-08-24 14:26:54 +1200

    define a new redux for ride info

---
+++ client/intelligent-ride-sharing-web-platform/src/apis/rideInfo.js
@@ -0,0 +1,50 @@
+// 行程相关的所有请求
+import { request } from "../utils";
+
+// 获取所有行程
+export function getRidesAPI() {
+    return request({
+        url: "/rides",
+        method: "GET"
+    });
+}
+
+// 根据 ID 获取行程
+export function getRideByIdAPI(id) {
+    return request({
+        url: `/rides/${id}`,
+        method: "GET"
+    });
+}
+
+// 创建行程
+export function createRideAPI(formData) {
+    return request({
+        url: "/rides",
+        method: "POST",
+        data: formData,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+// 更新行程
+export function updateRideAPI(id, formData) {
+    return request({
+        url: `/rides/${id}`,
+        method: "PUT",
+        data: formData,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+// 删除行程
+export function deleteRideAPI(id) {
+    return request({
+        url: `/rides/${id}`,
+        method: "DELETE"
+    });
+}
